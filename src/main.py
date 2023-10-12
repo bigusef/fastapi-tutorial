851d66bb3256b92f7a943ae9d7183adde8625784
Mahmud Youssef <mahmud.youssef@icloud.com>
2023-10-12 23:48:11 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,21 +6,7 @@ from .settings import get_settings
 
 settings = get_settings()
 
-app = FastAPI(
-    title="Wasla Mobile App",
-    description="""this application is a hub between online/offline stores from one side and delivery Individuals/companies from the other side. this platform helps a store to find various delivery options based on its location and find the best delivery deals. as a delivery entity, I search for the most attractive requests and try to finalize deals.""",
-    version="0.1.0",
-    terms_of_service="/legals",
-    license_info={"name": "Proprietary license"},
-    contact={
-        "name": "Appzone Technology",
-        "url": "https://appzone-technology.com",
-        "email": "info@appzone-technology.com",
-    },
-    openapi_url="/docs-files/v1.json",
-    # docs_url=None,
-    # redoc_url="/docs" if settings.is_debugging else None,
-)
+app = FastAPI()
 
 
 # region App Routers
